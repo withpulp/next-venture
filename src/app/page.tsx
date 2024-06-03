2e41621aaaa7a1ddec4e086db2fdea51408a29fb
Damir Vazgird <damir@withpulp.com>
2024-06-02 18:24:21 -0700

    chore: upgrade deps, revert link changes, cleanup home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import { Button, Hero, Main, Section } from "@/components"
+import { Hero, Main } from "@/components"
 
 const RootPage = () => (
   <Main className="container">
     <Hero />
-    <Section className="flex flex-row flex-wrap gap-4 md:gap-6 xl:gap-8">
-      <Button>Default Button</Button>
-      <Button color="primary">Primary Button</Button>
-      <Button color="secondary">Secondary Button</Button>
-      <Button color="success">Success Button</Button>
-      <Button color="warning">Warning Button</Button>
-      <Button color="danger">Danger Button</Button>
-    </Section>
   </Main>
 )
 
